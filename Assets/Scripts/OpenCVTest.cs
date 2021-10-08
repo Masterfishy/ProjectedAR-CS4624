0bf7f1eff3f751b9188a083e918c0293ca4163c3
zach <iamzachlong@gmail.com>
2021-10-08 13:26:09 -0400

    Uploaded plugin dlls and opencv test script

---
+++ Assets/Scripts/OpenCVTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class OpenCVTest : MonoBehaviour
+{
+    public int port = 1;
+
+    private bool _ready;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int result = OpenCVInterop.Init(ref port);
+        if (result == -1)
+        {
+            Debug.LogWarningFormat("[{0}] Failed to open the camera", GetType());
+            return;
+        }
+
+        _ready = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_ready)
+            return;
+
+        OpenCVInterop.Detect();
+    }
+
+    void OnApplicationQuit()
+    {
+        if (_ready)
+        {
+            OpenCVInterop.Close();
+        }
+    }
+}
+
+// Define the functions which can be called from the .dll.
+internal static class OpenCVInterop
+{
+    [DllImport("OpenCVArUco")]
+    internal static extern int Init(ref int capturePort);
+
+    [DllImport("OpenCVArUco")]
+    internal static extern int Close();
+
+    [DllImport("OpenCVArUco")]
+    internal static extern void Detect();
+}
