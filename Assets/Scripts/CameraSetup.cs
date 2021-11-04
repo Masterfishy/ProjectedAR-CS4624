6c5c1a391cb6865d46a064f24c21100e1291cd42
SidAkalwadi <35581882+SidAkalwadi@users.noreply.github.com>
2021-11-04 17:14:08 -0400

    Add files via upload
    Updated camera setup with public methods for panning and centering based on detected markers.

--- Assets/Scripts/CameraSetup.cs
+++ Assets/Scripts/CameraSetup.cs
@@ -9,15 +9,125 @@ public class CameraSetup : MonoBehaviour
     void Start()
     {
         cam.usePhysicalProperties = true; //physcial camera        
-        cam.transform.eulerAngles = new Vector3(-1.93f, 76.78f, 92.1f); //rotation
+        cam.transform.eulerAngles = new Vector3(-2f, 75.89f, 91.73f); //rotation
         cam.transform.position = new Vector3(0f, 0f, 0f); //position
-        cam.focalLength = 24.6f; //focal length
+        cam.focalLength = 26.8f; //focal length
         cam.sensorSize = new Vector2(16f, 9f); //aspect ratio
     }
+
+    private bool zooming = false;
+    private bool panX = false;
+    private bool panY = false;
+    private bool panZ = false;
+
+    private bool zoomDir = false;
+    private bool panXDir = false;
+    private bool panYDir = false;
+    private bool panZDir = false;
+
+    public float zoomSensitivity = 0.05f; //focal length per frame
+    public float panSensitivity = 0.1f; //vector angle per frame
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (zooming)
+        {
+            if (zoomDir) //true for zoom in, increase focal length
+                cam.focalLength += zoomSensitivity;
+            else
+                cam.focalLength -= zoomSensitivity; //false for zoom out, reduce focal length
+        }
+        if (panX)
+        {
+            if (panXDir) //true for left, increase X value
+            {
+                Vector3 newAngle = new Vector3(cam.transform.eulerAngles.x + panSensitivity, cam.transform.eulerAngles.y, cam.transform.eulerAngles.z);
+                cam.transform.eulerAngles = newAngle;
+            }
+            else
+            {
+                Vector3 newAngle = new Vector3(cam.transform.eulerAngles.x - panSensitivity, cam.transform.eulerAngles.y, cam.transform.eulerAngles.z);
+                cam.transform.eulerAngles = newAngle;
+            }
+        }
+        if (panY)
+        {
+            if (panXDir) //true for up, increase Y value
+            {
+                Vector3 newAngle = new Vector3(cam.transform.eulerAngles.x, cam.transform.eulerAngles.y + panSensitivity, cam.transform.eulerAngles.z);
+                cam.transform.eulerAngles = newAngle;
+            }
+            else
+            {
+                Vector3 newAngle = new Vector3(cam.transform.eulerAngles.x, cam.transform.eulerAngles.y - panSensitivity, cam.transform.eulerAngles.z);
+                cam.transform.eulerAngles = newAngle;
+            }
+        }
+        if (panZ)
+        {
+            if (panXDir) //true for clockwise, increase Z value
+            {
+                Vector3 newAngle = new Vector3(cam.transform.eulerAngles.x, cam.transform.eulerAngles.y, cam.transform.eulerAngles.z + panSensitivity);
+                cam.transform.eulerAngles = newAngle;
+            }
+            else
+            {
+                Vector3 newAngle = new Vector3(cam.transform.eulerAngles.x, cam.transform.eulerAngles.y, cam.transform.eulerAngles.z- panSensitivity);
+                cam.transform.eulerAngles = newAngle;
+            }
+        }
+    }
+
+    // Starts or stops zooming in the given direction
+    public void zoomControl(bool running, bool direction)
+    {
+        zooming = running;
+        zoomDir = direction;
+    }
+
+    public void panXControl(bool running, bool direction)
+    {
+        panX = running;
+        panXDir = direction;
+    }
+
+    public void panYControl(bool running, bool direction)
+    {
+        panY = running;
+        panYDir = direction;
+    }
+
+    public void panZControl(bool running, bool direction)
+    {
+        panZ = running;
+        panZDir = direction;
+    }
+
+    // takes in an aruco ID and pans x and y accordingly
+    public void centerShotgun(int id)
+    {
+        //convert id to 0 based first, starting ID in the shotgun marker matrix is 4
+        id -= 4;
+        int row = id / 5; //5x5 matrix starting at top left and going down
+        int col = id % 5; 
+
+        if(row != 3)
+        {
+            panXControl(true, row < 3); //less than 3 means go to the left, panX control takes true to go to the left
+        }
+        else
+        {
+            panXControl(false, false); //stop panning X
+        }
+
+        if (col != 3)
+        {
+            panYControl(true, col < 3); //less than 3 means go up, panY control takes true to go up
+        }
+        else
+        {
+            panYControl(false, false); //stop panning Y
+        }
     }
 }
