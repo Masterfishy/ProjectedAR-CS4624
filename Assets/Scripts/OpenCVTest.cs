dde3707c9c181fa5ea815327fbc8a20d4c98db10
Zach Long <iamzachlong@gmail.com>
2021-11-05 14:01:39 -0400

    A bunch of stuff ok, don't at me

--- Assets/Scripts/OpenCVTest.cs
+++ Assets/Scripts/OpenCVTest.cs
@@ -3,14 +3,99 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
 
+// Good azure camera expoure: 16670, lights at 2 dots
+
 public class OpenCVTest : MonoBehaviour
 {
     public int port = 0;
+    public CameraSetup cameraCtrl;
 
-    private bool _ready;
+    private bool _ready = false;
 
     // Start is called before the first frame update
     void Start()
+    {
+        CaptureCamera();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_ready)
+            return;
+
+        // Detect markers and draw on borders
+        OpenCVInterop.Detect();
+
+        double xRes = OpenCVInterop.xDistance();
+        // If xRes is negative, marker 0 is right of marker 3
+        if (xRes > 0)
+        {
+            // Go left
+            cameraCtrl.panXControl(true, true);
+        }
+        else if (xRes < 0)
+        {
+            // Go right
+            cameraCtrl.panXControl(true, false);
+        }
+        else
+        {
+            // Don't move on the x axis
+            Debug.Log("Stop panning X");
+            cameraCtrl.panXControl(false, false);
+        }
+
+        double yRes = OpenCVInterop.yDistance();
+        // If yRes is negative, marker 1 is below marker 3
+        if (yRes > 0)
+        {
+            // Go up
+            cameraCtrl.panYControl(true, true);
+        }
+        else if (yRes < 0)
+        {
+            // Go down
+            cameraCtrl.panYControl(true, false);
+        }
+        else
+        {
+            // Don't move on the y axis
+            Debug.Log("Stop panning Y");
+            cameraCtrl.panYControl(false, false);
+        }
+
+        Debug.Log($"X Distance: {xRes}");
+        Debug.Log($"Y Distance: {yRes}");
+    }
+
+    void OnApplicationQuit()
+    {
+        if (_ready)
+        {
+            OpenCVInterop.Close();
+        }
+    }
+
+    void OnEnable()
+    {
+        if (!_ready) //add check to see if camera is open by another application, if yes, return error to make sure unity doesn't crashf
+        {
+            CaptureCamera();
+        }
+    }
+
+    void OnDisable()
+    {
+        if (_ready)
+        {
+            OpenCVInterop.Close();
+
+            _ready = false;
+        }
+    }
+
+    private void CaptureCamera()
     {
         int result = OpenCVInterop.Init(ref port);
         if (result == -1)
@@ -20,23 +105,6 @@ public class OpenCVTest : MonoBehaviour
         }
 
         _ready = true;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (!_ready)
-            return;
-
-        OpenCVInterop.Detect();
-    }
-
-    void OnApplicationQuit()
-    {
-        if (_ready)
-        {
-            OpenCVInterop.Close();
-        }
     }
 }
 
@@ -51,4 +119,10 @@ internal static class OpenCVInterop
 
     [DllImport("CalibrationCamera")]
     internal static extern void Detect();
+
+    [DllImport("CalibrationCamera")]
+    internal static extern double xDistance();
+
+    [DllImport("CalibrationCamera")]
+    internal static extern double yDistance();
 }
--- CalibrationCamera/Source.cpp
+++ CalibrationCamera/Source.cpp
@@ -1,25 +1,38 @@
-// Import the aruco module in OpenCV
+// Import OpenCV modules
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/aruco.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/imgproc.hpp>
+
 #include <iostream>
+#include <algorithm>
+#include <cmath>
 
 cv::VideoCapture _capture;
 cv::Ptr<cv::aruco::Dictionary> _dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_7X7_50);
+
+struct DataHolder {
+	DataHolder() {}
+	double xDiff, yDiff;
+};
 
 double calculateDist(double x1, double y1, double x2, double y2);
 cv::Point2f findMidpoint(cv::Point2f p1, cv::Point2f p2);
 
 int main()
 {
-	_capture.open(0);
+	try {
+		_capture.open(0);
+	}
+	catch (const std::exception e) {
+		return 1;
+	}
 
 	while (_capture.grab())
 	{
 		cv::Mat image, imageCopy;
-		_capture.retrieve(image);
+		_capture.retrieve(image); 
 		image.copyTo(imageCopy);
 
 		std::vector<int> ids;
@@ -27,32 +40,147 @@ int main()
 		cv::aruco::detectMarkers(image, _dictionary, corners, ids);
 
 		std::vector<std::vector<cv::Point2f>> sortedCorners{
-			std::vector<cv::Point2f>(),
-			std::vector<cv::Point2f>(),
-			std::vector<cv::Point2f>(),
-			std::vector<cv::Point2f>()
+			std::vector<cv::Point2f>(4),
+			std::vector<cv::Point2f>(4),
+			std::vector<cv::Point2f>(4),
+			std::vector<cv::Point2f>(4)
 		};
+
+		bool sees0 = false, sees1 = false, sees2 = false, sees3 = false;
 
 		for (int i = 0; i < corners.size(); i++) {
 			if (ids[i] == 0) {
 				sortedCorners[0] = corners[i];
+				sees0 = true;
 			}
 
 			if (ids[i] == 1) {
 				sortedCorners[1] = corners[i];
+				sees1 = true;
 			}
 
 			if (ids[i] == 2) {
 				sortedCorners[2] = corners[i];
+				sees2 = true;
 			}
 
 			if (ids[i] == 3) {
 				sortedCorners[3] = corners[i];
+				sees3 = true;
 			}
 		}
 
 		// if at least one marker is detected
-		if (ids.size() > 1)
+
+		if (ids.size() > 0)
+		{
+
+			cv::aruco::drawDetectedMarkers(imageCopy, corners, ids);
+			double xDiff = abs(sortedCorners[0][1].x - sortedCorners[3][1].x);
+			double yDiff = abs(sortedCorners[1][2].y - sortedCorners[3][2].y);
+
+			std::cout << "X Diff: " << xDiff << ", Y Diff: " << yDiff << std::endl;
+
+			std::cout << "ID " << 0 << ", top right: " << sortedCorners[0][1] << std::endl;
+			std::cout << "ID " << 3 << ", top right: " << sortedCorners[3][1] << std::endl;
+			std::cout << "ID " << 1 << ", bot right: " << sortedCorners[1][2] << std::endl;
+			std::cout << "ID " << 3 << ", bot right: " << sortedCorners[3][2] << std::endl;
+
+			std::cout << "ID " << 0 << ": " << sortedCorners[0] << std::endl;
+			std::cout << "ID " << 1 << ": " << sortedCorners[1] << std::endl;
+			std::cout << "ID " << 2 << ": " << sortedCorners[2] << std::endl;
+			std::cout << "ID " << 3 << ": " << sortedCorners[3] << std::endl;
+
+			std::cout << "ID " << 0 << ", arc length: " << cv::arcLength(sortedCorners[0], true) << std::endl;
+			std::cout << "ID " << 1 << ", arc length: " << cv::arcLength(sortedCorners[1], true) << std::endl;
+			std::cout << "ID " << 2 << ", arc length: " << cv::arcLength(sortedCorners[2], true) << std::endl;
+			std::cout << "ID " << 3 << ", arc length: " << cv::arcLength(sortedCorners[3], true) << std::endl;
+
+			//0 to 3
+			/*
+			* Calculate movement on X
+			* 
+			* Bottom right marker and top right marker
+			*
+			* moveXamt = abs(topRightId0.x - topRightId3.x)
+			*/
+
+			/*cv::Point2f topRightId0 = sortedCorners[0][1];
+			cv::Point2f topRightId3 = sortedCorners[3][1];
+
+			cv::line(imageCopy, topRightId0, topRightId3, cv::Scalar(0, 0, 255), 1);
+			dist = calculateDist(topRightId3.x, topRightId3.y, topRightId0.x, topRightId0.y);
+			std::cout << dist << "px" << std::endl;*/
+
+			
+
+			/*
+			* Calculate movement on Y
+			* 
+			* Top left marker and top right marker
+			* 
+			* moveYamt = abs(bottomRightId1.y - bottomRightId3.y)
+			*/
+			
+			/*cv::Point2f bottomRightId1 = sortedCorners[1][2];
+			cv::Point2f bottomRightId3 = sortedCorners[3][2];
+
+			cv::line(imageCopy, bottomRightId1, bottomRightId3, cv::Scalar(0, 0, 255), 1);
+			dist = calculateDist(bottomRightId3.x, bottomRightId3.y, bottomRightId1.x, bottomRightId1.y);
+			std::cout << dist << "px" << std::endl;*/
+
+			
+			/*std::string distText = std::to_string(dist) + " px.";
+			cv::Point2f midpoint = findMidpoint(topRightId0, topRightId3);
+
+			cv::putText(imageCopy, distText, midpoint, cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 0, 255), 1, true);*/
+
+
+			//cv::Point2f topLeftId0 = sortedCorners[0][0];
+			//cv::Point2f topLeftId3 = sortedCorners[3][0];
+
+			//cv::line(imageCopy, topLeftId0, topLeftId3, cv::Scalar(0, 0, 255), 1);
+			//dist = calculateDist(topLeftId3.x, topLeftId3.y, topLeftId0.x, topLeftId0.y);
+			//std::cout << dist << "px" << std::endl;
+
+			/*distText = std::to_string(dist) + " px.";
+			midpoint = findMidpoint(topLeftId0, topLeftId3);
+
+			cv::putText(imageCopy, distText, midpoint, cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 0, 255), 1, true);*/
+
+
+			/*
+			* Top left marker and top right marker
+			*
+			* moveYamt = abs(bottomRightId1.y - bottomRightId3.y)
+			*/
+			//cv::Point2f topRightId1 = sortedCorners[1][1];
+			////cv::Point2f topRightId3 = sortedCorners[3][1];
+
+			//cv::line(imageCopy, topRightId1, topRightId3, cv::Scalar(0, 0, 255), 1);
+			//dist = calculateDist(topRightId3.x, topRightId3.y, topRightId1.x, topRightId1.y);
+			//std::cout << dist << "px" << std::endl;
+
+			/*distText = std::to_string(dist) + " px.";
+			midpoint = findMidpoint(topRightId1, topRightId3);*/
+
+
+
+			/*cv::aruco::drawDetectedMarkers(imageCopy, corners, ids);
+
+			cv::line(imageCopy, sortedCorners[0][0], sortedCorners[1][2], cv::Scalar(0, 0, 255), 1);
+			dist = calculateDist(sortedCorners[0][0].x, sortedCorners[0][0].y, sortedCorners[1][2].x, sortedCorners[1][2].y);
+			std::cout << dist << "px" << std::endl;
+
+			std::string distText = std::to_string(dist) + " px.";
+			cv::Point2f midpoint = findMidpoint(sortedCorners[0][0], sortedCorners[1][2]);
+
+			cv::putText(imageCopy, distText, midpoint, cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 0, 255), 1, true);*/
+		}
+		/*bool aruco0and1 = std::find(ids.begin(), ids.end(), 0) != ids.end() && std::find(ids.begin(), ids.end(), 1) != ids.end();
+
+		double dist = -1.0;
+		if (ids.size() > 1 && aruco0and1)
 		{
 			cv::aruco::drawDetectedMarkers(imageCopy, corners, ids);
 
@@ -64,7 +192,7 @@ int main()
 			cv::Point2f midpoint = findMidpoint(sortedCorners[0][0], sortedCorners[1][2]);
 
 			cv::putText(imageCopy, distText, midpoint, cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 0, 255), 1, true);
-		}
+		}*/
 
 		cv::imshow("out", imageCopy);
 		char key = (char)cv::waitKey(0);
@@ -84,12 +212,17 @@ cv::Point2f findMidpoint(cv::Point2f p1, cv::Point2f p2)
 	return (p1 + p2) * 0.5;
 }
 
-
 extern "C" int __declspec(dllexport) __stdcall Init(int& capturePort)
 {
-	_capture.open(capturePort);
+	try {
+		_capture.open(capturePort);
+	}
+	catch (const std::exception e) {
+		return 1;
+	}
+
 	if (!_capture.isOpened())
-		return -1;
+		return 2;
 
 	return 0;
 }
@@ -118,4 +251,222 @@ extern "C" void __declspec(dllexport) __stdcall Detect()
 	}
 
 	cv::imshow("out", imageCopy);
+}
+
+extern "C" double __declspec(dllexport) __stdcall xDistance()
+{
+	_capture.grab();
+
+	cv::Mat image, imageCopy;
+	_capture.retrieve(image);
+	image.copyTo(imageCopy);
+
+	std::vector<int> ids;
+	std::vector<std::vector<cv::Point2f>> corners;
+	cv::aruco::detectMarkers(image, _dictionary, corners, ids);
+
+	std::vector<std::vector<cv::Point2f>> sortedCorners{
+		std::vector<cv::Point2f>(4),
+		std::vector<cv::Point2f>(4),
+		std::vector<cv::Point2f>(4),
+		std::vector<cv::Point2f>(4)
+	};
+
+	bool sees0 = false, sees1 = false, sees2 = false, sees3 = false;
+
+	for (int i = 0; i < corners.size(); i++) {
+		if (ids[i] == 0) {
+			sortedCorners[0] = corners[i];
+			sees0 = true;
+		}
+
+		if (ids[i] == 1) {
+			sortedCorners[1] = corners[i];
+			sees1 = true;
+		}
+
+		if (ids[i] == 2) {
+			sortedCorners[2] = corners[i];
+			sees2 = true;
+		}
+
+		if (ids[i] == 3) {
+			sortedCorners[3] = corners[i];
+			sees3 = true;
+		}
+	}
+
+	if (sees0 && sees3)
+	{
+		return (double)sortedCorners[3][1].x - (double)sortedCorners[0][1].x;
+	}
+
+	return 1;
+}
+
+extern "C" double __declspec(dllexport) __stdcall yDistance()
+{
+	_capture.grab();
+
+	cv::Mat image, imageCopy;
+	_capture.retrieve(image);
+	image.copyTo(imageCopy);
+
+	std::vector<int> ids;
+	std::vector<std::vector<cv::Point2f>> corners;
+	cv::aruco::detectMarkers(image, _dictionary, corners, ids);
+
+	std::vector<std::vector<cv::Point2f>> sortedCorners{
+		std::vector<cv::Point2f>(4),
+		std::vector<cv::Point2f>(4),
+		std::vector<cv::Point2f>(4),
+		std::vector<cv::Point2f>(4)
+	};
+
+	bool sees0 = false, sees1 = false, sees2 = false, sees3 = false;
+
+	for (int i = 0; i < corners.size(); i++) {
+		if (ids[i] == 0) {
+			sortedCorners[0] = corners[i];
+			sees0 = true;
+		}
+
+		if (ids[i] == 1) {
+			sortedCorners[1] = corners[i];
+			sees1 = true;
+		}
+
+		if (ids[i] == 2) {
+			sortedCorners[2] = corners[i];
+			sees2 = true;
+		}
+
+		if (ids[i] == 3) {
+			sortedCorners[3] = corners[i];
+			sees3 = true;
+		}
+	}
+
+	if (sees1 && sees3)
+	{
+		return (double)sortedCorners[3][2].y - (double)sortedCorners[1][2].y;
+	}
+
+	return 1;
+}
+
+extern "C" double __declspec(dllexport) __stdcall ScaleDifference()
+{
+	_capture.grab();
+
+	cv::Mat image, imageCopy;
+	_capture.retrieve(image);
+	image.copyTo(imageCopy);
+
+	std::vector<int> ids;
+	std::vector<std::vector<cv::Point2f>> corners;
+	cv::aruco::detectMarkers(image, _dictionary, corners, ids);
+
+	std::vector<std::vector<cv::Point2f>> sortedCorners{
+		std::vector<cv::Point2f>(4),
+		std::vector<cv::Point2f>(4),
+		std::vector<cv::Point2f>(4),
+		std::vector<cv::Point2f>(4)
+	};
+
+	bool sees0 = false, sees1 = false, sees2 = false, sees3 = false;
+
+	for (int i = 0; i < corners.size(); i++) {
+		if (ids[i] == 0) {
+			sortedCorners[0] = corners[i];
+			sees0 = true;
+		}
+
+		if (ids[i] == 1) {
+			sortedCorners[1] = corners[i];
+			sees1 = true;
+		}
+
+		if (ids[i] == 2) {
+			sortedCorners[2] = corners[i];
+			sees2 = true;
+		}
+
+		if (ids[i] == 3) {
+			sortedCorners[3] = corners[i];
+			sees3 = true;
+		}
+	}
+
+	if (sees1 && sees3)
+	{
+	}
+
+	return 1;
+}
+
+extern "C" void __declspec(dllexport) __stdcall MeasureArucoDistance(DataHolder* outHolder)
+{
+	_capture.grab();
+
+	cv::Mat image, imageCopy;
+	_capture.retrieve(image);
+	image.copyTo(imageCopy);
+
+	std::vector<int> ids;
+	std::vector<std::vector<cv::Point2f>> corners;
+	cv::aruco::detectMarkers(image, _dictionary, corners, ids);
+
+	std::vector<std::vector<cv::Point2f>> sortedCorners{
+		std::vector<cv::Point2f>(),
+		std::vector<cv::Point2f>(),
+		std::vector<cv::Point2f>(),
+		std::vector<cv::Point2f>()
+	};
+
+	for (int i = 0; i < corners.size(); i++) {
+		if (ids[i] == 0) {
+			sortedCorners[0] = corners[i];
+		}
+
+		if (ids[i] == 1) {
+			sortedCorners[1] = corners[i];
+		}
+
+		if (ids[i] == 2) {
+			sortedCorners[2] = corners[i];
+		}
+
+		if (ids[i] == 3) {
+			sortedCorners[3] = corners[i];
+		}
+	}
+
+	// If at least one marker is detected
+	if (ids.size() > 0) {
+		cv::aruco::drawDetectedMarkers(imageCopy, corners, ids);
+	}
+
+	bool aruco0and1 = std::find(ids.begin(), ids.end(), 0) != ids.end() && std::find(ids.begin(), ids.end(), 1) != ids.end() && std::find(ids.begin(), ids.end(), 3) != ids.end();
+		
+	if (ids.size() > 2 && aruco0and1)
+	{
+
+		cv::Point2f topRightId0 = sortedCorners[0][1];
+		cv::Point2f topRightId3 = sortedCorners[3][1];
+
+		double x = std::abs(topRightId0.x - topRightId3.x);
+		cv::line(imageCopy, topRightId0, topRightId3, cv::Scalar(0, 0, 255), 1);
+
+		cv::Point2f bottomRightId1 = sortedCorners[1][2];
+		cv::Point2f bottomRightId3 = sortedCorners[3][2];
+
+		double y = std::abs(bottomRightId1.y - bottomRightId3.y);
+		cv::line(imageCopy, bottomRightId1, bottomRightId3, cv::Scalar(0, 0, 255), 1);
+
+		outHolder->xDiff = x;
+		outHolder->yDiff = y;
+	}
+
+	cv::imshow("yo whats up guys, smash that like and subscribe button and turn on notifications for more calibration content! 8)", imageCopy);
 }
